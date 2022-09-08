a764017e70735f579724f288eb2a070a1c7a6772
CristiScarlat <78.cristi@gmail.com>
2022-09-08 21:42:32 +0300

    add video clip from firebase

--- pages/galery.js
+++ pages/galery.js
@@ -8,10 +8,16 @@ const Galery = () => {
     const photoArray = new Array(galery.qty).fill(galery.baseFotoName).map((elem, index) => elem + index);
 
     return (
-        <div className={styles["galery-wrapper"]}>
-            {photoArray.map(photo => (
-                <Image key={photo} src={`/galery/${photo}.jpeg`} alt="galery item" width={320} height={320} />
-            ))}
+        <div style={{textAlign: 'center'}}>
+            <video autoPlay loop controls width="80%">
+                <source src="https://firebasestorage.googleapis.com/v0/b/cris-web-apps.appspot.com/o/dermatiq%2FDermatIQ.mp4?alt=media&token=8939a276-f9d7-4984-89e3-df76842dd10a" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div className={styles["galery-wrapper"]}>
+                {photoArray.map(photo => (
+                    <Image key={photo} src={`/galery/${photo}.jpeg`} alt="galery item" width={320} height={320} />
+                ))}
+            </div>
         </div>
     )
 }
